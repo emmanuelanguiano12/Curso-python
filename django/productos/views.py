8fd21e2c3af0f284161fe633049f6f97e90d1367
emmanuelanguiano12 <emmanuelanguiano1223@gmail.com>
2024-08-16 13:46:17 -0600

    Django App

--- django/productos/views.py
+++ django/productos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from .forms import ProductoForm
 from .models import Producto
 
 # Create your views here.
@@ -12,4 +13,28 @@ def index(request):
         request,
         'index.html',
         context={ 'productos': productos }
+    )
+
+def detalle(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+
+    return render(
+        request, 
+        'detalle.html', 
+        context={'producto': producto}
+    )
+
+def formulario(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/productos')
+    else:
+        form = ProductoForm()
+
+    return render(
+        request,
+        'producto_form.html',
+        {'form': form}
     )
